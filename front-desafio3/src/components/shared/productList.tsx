585eb568c02006e8be8eee9631226f9eda499f77
Eduardo <eeadomingues@gmail.com>
2024-10-23 02:21:43 -0300

    feat: add product card hover

--- front-desafio3/src/components/shared/productList.tsx
+++ front-desafio3/src/components/shared/productList.tsx
@@ -1,6 +1,10 @@
 // src/components/ProductList.tsx
 import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import axios from "axios";
+import { BsShareFill } from "react-icons/bs";
+import { GoArrowSwitch } from "react-icons/go";
+import { IoMdHeartEmpty } from "react-icons/io";
 
 interface Product {
   id: number;
@@ -28,7 +32,7 @@ const ProductList: React.FC = () => {
   return (
     <div className="mt-6 grid grid-cols-4 gap-6 bg-white py-8">
       {products.map((product) => (
-        <div key={product.id} className="relative bg-gray-100">
+        <div key={product.id} className="product-card relative bg-gray-100">
           <div className="absolute right-2 top-2 flex flex-row">
             {product.isNew && (
               <span className="flex h-12 w-12 items-center justify-center rounded-full border bg-green-500 text-sm text-white">
@@ -65,6 +69,28 @@ const ProductList: React.FC = () => {
                 })}
               </span>
             )}
+          </div>
+          <div className="hover-content">
+            <Link
+              to={`/products/${product.id}`}
+              className="details-button w-1-2 text-card-button py-2-5 mb-4 flex justify-center bg-white px-5"
+            >
+              See Details
+            </Link>
+            <div className="w-4-5 flex justify-between">
+              <button className="flex flex-row items-center justify-center gap-1">
+                <BsShareFill />
+                Share
+              </button>
+              <button className="flex flex-row items-center justify-center gap-1">
+                <GoArrowSwitch />
+                Compare
+              </button>
+              <button className="flex flex-row items-center justify-center gap-1">
+                <IoMdHeartEmpty />
+                Like
+              </button>
+            </div>
           </div>
         </div>
       ))}
